60b5702d0403d5c8e87fed0a4fc4c734c1d2f380
Ronit Prasad <55261813+RonitPrasad1@users.noreply.github.com>
2021-10-24 23:51:07 -0500

    Update Candy Shop.cpp

--- Candy Shop.cpp
+++ Candy Shop.cpp
@@ -16,7 +16,7 @@ int main()
     for (int i = 0; i < n; ++i)
     {
         std::cin >> a >> b;
-        if ((b == k || k == b))
+        if ( (b == k || k == b) )
         {
             ++counter1;
         }
