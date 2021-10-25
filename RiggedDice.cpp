e73913d7af39845fc118595858cd8878a541135f
Ronit Prasad <55261813+RonitPrasad1@users.noreply.github.com>
2021-10-24 23:49:21 -0500

    Update RiggedDice.cpp

--- RiggedDice.cpp
+++ RiggedDice.cpp
@@ -1,38 +1,37 @@
 //Joseph: 75/75 (Full)
 
 #include <iostream>
-using namespace std;
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int T = 0;
-    cin >> T;
+    int T, N, diePos, die1Tot, die2Tot, aScore, bScore;
+    std::cin >> T;
     
-    for(int i = 0; i < T; i++)
+    for (int i = 0; i < T; i++)
     {
-        int N = 0;
-        cin >> N;
+        std::cin >> N;
         
-        int diePos = 1, die1Tot = 0, die2Tot = 0;
-        int aScore = 0, bScore = 0;
+        diePos = 1, die1Tot = 0, die2Tot = 0;
+        aScore = 0, bScore = 0;
         
-        for(int j = 0; j < N; j++)
+        for (int j = 0; j < N; j++)
         {
             int A = 0, B = 0;
-            cin >> A >> B;
+            std::cin >> A >> B;
+            
             aScore += A;
             bScore += B;
             
-            if(aScore != bScore && diePos == 1)
+            if (aScore != bScore && diePos == 1)
             {
                 die1Tot += A;
                 die2Tot += B;
                 diePos = 2;
             }
-            else if(aScore != bScore && diePos == 2)
+            else if (aScore != bScore && diePos == 2)
             {
                 die1Tot += B;
                 die2Tot += A;
@@ -41,15 +40,14 @@ int main()
             
         }
         
-        if(die1Tot > die2Tot)
+        if (die1Tot > die2Tot)
         {
-            cout << 1 << endl;
+            std::cout << 1 << endl;
         }
         else
         {
-            cout << 2 << endl;
+            std::cout << 2 << endl;
         }
     }
-    
     return 0;
 }
